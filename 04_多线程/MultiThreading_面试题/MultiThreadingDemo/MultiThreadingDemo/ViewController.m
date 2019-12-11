7182917cace11e08385db2e970bd9ba094268fd2
Jentle <zhijt@yiche.com>
2019-12-11 23:40:29 +0800

    gcd的几个面试题

---
+++ 04_多线程/MultiThreading_面试题/MultiThreadingDemo/MultiThreadingDemo/ViewController.m
@@ -0,0 +1,121 @@
+//
+//  ViewController.m
+//  MultiThreadingDemo
+//
+//  Created by Jentle on 2019/12/11.
+//  Copyright © 2019 Jentle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    [self interview06];
+}
+
+- (void)interview01{
+    ///问题：以下代码是在主线程执行，会不会产生死锁？
+    ///会产生死锁
+    NSLog(@"任务1");
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    dispatch_sync(queue, ^{
+        NSLog(@"任务2");
+    });
+    NSLog(@"任务3");
+}
+
+- (void)interview02{
+    ///问题：以下代码是在主线程执行，会不会产生死锁？
+    ///不会产生死锁
+    NSLog(@"任务1");
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    dispatch_async(queue, ^{
+        NSLog(@"任务2");
+    });
+    NSLog(@"任务3");
+}
+
+- (void)interview03{
+    ///问题：以下代码是在主线程执行，会不会产生死锁？
+    ///会产生死锁
+    NSLog(@"任务1");
+    dispatch_queue_t queue = dispatch_queue_create("com.bitauto.cxt", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(queue, ^{///block0
+        NSLog(@"任务2");
+        dispatch_sync(queue, ^{///block1
+            NSLog(@"任务3");
+        });
+        NSLog(@"任务4");
+    });
+    NSLog(@"任务5");
+    ///任务1 -> 任务5 -> 任务2 -> 崩溃
+}
+
+- (void)interview04{
+    ///问题：以下代码是在主线程执行，会不会产生死锁？
+    ///不会产生死锁
+    NSLog(@"任务1");
+    dispatch_queue_t queue = dispatch_queue_create("com.bitauto.cxt", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t queue0 = dispatch_queue_create("com.bitauto.cxt", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{///block0
+        NSLog(@"任务2");
+        dispatch_sync(queue0, ^{///block1
+            NSLog(@"任务3");
+        });
+        NSLog(@"任务4");
+    });
+    NSLog(@"任务5");
+    ///任务1 -> 任务5 -> 任务2 -> 任务3 -> 任务4
+}
+
+- (void)interview05{
+    ///问题：以下代码是在主线程执行，会不会产生死锁？
+    ///不会产生死锁
+    NSLog(@"任务1");
+    dispatch_queue_t queue = dispatch_queue_create("com.bitauto.cxt", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{///block0
+        NSLog(@"任务2");
+        dispatch_sync(queue, ^{///block1
+            NSLog(@"任务3");
+        });
+        NSLog(@"任务4");
+    });
+    NSLog(@"任务5");
+    ///任务1 -> 任务5 -> 任务2 -> 任务3 -> 任务4
+}
+
+- (void)interview06{
+    
+//    NSLog(@"------------主线程-----------------");
+//    NSLog(@"1");
+//    ///afterDelay：底层是定时器，添加到runLoop中，runLoop休眠前处理任务。
+//    [self performSelector:@selector(printNumber2) withObject:nil afterDelay:.0];/// 输出 1 3 2
+//    NSLog(@"3");
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    dispatch_async(queue, ^{
+        NSLog(@"1");
+//        [self performSelector:@selector(printNumber2) withObject:nil];/// 输出 1 2 3
+        ///这句代码的本质是往RunLoop中添加定时器，子线程默认没有开启RunLoop
+        [self performSelector:@selector(printNumber2) withObject:nil afterDelay:.0];/// 输出 1 3
+        NSLog(@"3");
+        [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init]forMode:NSDefaultRunLoopMode];
+        [[NSRunLoop currentRunLoop] run];
+    });
+}
+
+- (void)printNumber2 {
+    
+    NSLog(@"2");
+}
+
+@end
