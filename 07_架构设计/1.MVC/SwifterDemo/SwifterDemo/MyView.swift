b87e6aebbacdd7431c2f35737813efb878c8a259
Jentle <zhijt@yiche.com>
2020-03-04 15:17:59 +0800

    update

---
+++ 07_架构设计/1.MVC/SwifterDemo/SwifterDemo/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  SwifterDemo
+//
+//  Created by Jentle on 2019/12/25.
+//  Copyright © 2019 Jentle. All rights reserved.
+//
+
+import UIKit
+
+class CXTAboutView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ 07_架构设计/1.MVC/SwifterDemo/SwifterDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwifterDemo
+//
+//  Created by Jentle on 2019/12/25.
+//  Copyright © 2019 Jentle. All rights reserved.
+//
+
+import UIKit
+
+class CYTAboutViewController: UIViewController {
+    
+    override func loadView() {
+        view = CXTAboutView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
