0ee1944fefd83f4b863550ee545f55ca9de5c175
Jentle <zhijt@yiche.com>
2021-01-25 19:41:29 +0800

    runLoop补充

---
+++ CFSource/CFOpenSource/CFOpenSource/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CFOpenSource
+//
+//  Created by Jentle on 2021/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
